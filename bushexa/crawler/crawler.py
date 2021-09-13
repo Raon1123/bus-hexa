89083af52e886ea26813ea690e6197aef0004bfd
RaonMire <anyunpyo@unist.ac.kr>
2021-09-13 20:55:35 +0900

    crawler

--- bushexa/crawler/crawler.py
+++ bushexa/crawler/crawler.py
@@ -1,42 +1,130 @@
 import requests
 import xmltodict
 import json
+import time
+
+error_log_path = './log.txt'
 
 """
-Ulsan API에게 정보를 요청하는 부분
+API key를 읽는 함수
+
 Input
-- stopid: 버스정류장 번호
+- path: API key의 위치, 기본은 bushexa/crawler/key.txt
+
 Output
-- 
+- key: API key
+
 """
-def request_API(stopid='196040122'):
-    key_file = open('./key.txt', 'r')
+def get_key(path='./key.txt'):
+    key_file = open(path, 'r')
+
     key = key_file.read()
+    key = requests.utils.unquote(key)
 
-    url = 'http://openapi.its.ulsan.kr/UlsanAPI/getBusArrivalInfo.xo'
+    key_file.close()
 
-    params = {'stopid': stopid,
-            'pageNo': '1',
-            'numOfRows': '10',
-            'serviceKey': key}
+    return key
 
-    print(url)
+
+"""
+Ulsan API에게 출발시간 정보를 요청하는 부분
+
+Input
+- route: 버스번호 eg) 133, 233
+- day: 요일구분, 0-평일, 1-토요일, 2-주말,공휴일, 3-방학평일, 4-방학토요일, 5-방학일요일
+- page: 요청 페이지
+
+Output
+- xml: 받은 xml 형식의 파일
+"""
+def request_time(route, day=0, page=1, row=10):
+    # Check input data type
+    if type(route) is not str:
+        route = str(route)
+    
+    if type(day) is not str:
+        day = str(day)
+
+    if type(page) is not str:
+        page = str(page)
+
+    if type(row) is not str:
+        row = str(row)
+
+    # Get API key
+    key = get_key()
+
+    # Timetalbe URL
+    url = 'http://openapi.its.ulsan.kr/UlsanAPI/BusTimetable.xo'
+
+    # API parameter
+    params = {'serviceKey': key,
+              'pageNo': page,
+              'numOfRows': row, 
+              'routeNo': route,
+              'dayOfWeek': day}
+
+    # Request to API
     response = requests.get(url, params=params)
-    print(response.status_code)
-    print(response.text)
+    
+    # Fail timetable request
+    if response.status_code is not 200:
+        time_msg = time.strftime('%Y-%m-%d %I:%M:%S %p', time.localtime())
+        msg = "Fail Time Request! route:" + route + " day: " + day + "TIME: " + time_msg
+        raise Exception(msg)
+
+    # Get response text
     xml = response.text
 
     return xml
 
+
 def parse_xml(xml):
     js = xmltodict.parse(xml)
-    info_dict = json.loads(json.dumps(js))['tableInfo']['list']['row']
-    return info_dict
+    js_dict = json.loads(json.dumps(js))
+    cnt = int(js_dict['tableInfo']['totalCnt'])
+    info_dict = js_dict['tableInfo']['list']['row']
+
+    return cnt, info_dict
+
+
+def crawl_time():
+    routes = [133, 733, 743]
+    timetable = {}
+    err_file = open(error_log_path, 'a')
+
+    for route in routes:
+        row = 10
+        page = 1
+
+        timetable[route] = {'1':[], '2':[]}
+
+        while True:
+            try:
+                time_xml = request_time(route=route, page=page, row=row)
+
+                tot, info = parse_xml(time_xml)
+
+                for r in info:
+                    dir = r['DIRECTION']
+                    timestr = r['TIME']
+                    timetable[route][dir].append(timestr)
+
+                page = page + 1
+                
+
+            except Exception as e:
+                err_file.write("ERROR", e)
+                break
+
+            if page * row < tot:
+                continue
+
+            break
+
+    print(timetable)
+    err_file.close()
 
 
 if __name__=='__main__':
-    # xml = request_API()
-    xml_file = open('./test.xml', 'r')
-    xml = xml_file.read()
-    d = parse_xml(xml)
-    print(d[0])
+    crawl_time()
