fa1b53d62d5e4d319a18ec1d14b3068931f3269d
jaehwan1912 <jimshim@unist.ac.kr>
2021-10-24 22:19:43 +0900

    New requestor : pseudo-parallel GET requests

--- bushexa/chroniccrawler/crawler/tools/requestor.py
+++ bushexa/chroniccrawler/crawler/tools/requestor.py
@@ -1,8 +1,39 @@
 import requests
 import xmltodict
+import asyncio
+import aiohttp
 
+
+# Async task for getting response from thing_url_params and session
+async def getit(t_u_p):
+    async with aiohttp.request('GET', t_u_p[1], params=t_u_p[2]) as response:
+        text = await response.text()
+    if response.status != 200:
+        raise Exception("Status not ok...")
+    return (t_u_p[0], text)
+
+# Get all
+async def getall(t_u_ps):
+    tasks = [asyncio.create_task(getit(t_u_p)) for t_u_p in t_u_ps]
+    thing_responses = await asyncio.gather(*tasks)
+
+    return thing_responses
 
 # Returns the response converted into a dictionary
+def request_dicts(t_u_ps):
+    thing_texts = asyncio.run(getall(t_u_ps))
+
+    thing_rdicts = []
+
+    for t_r in thing_texts:
+        d = xmltodict.parse(t_r[1])
+        thing_rdicts.append((t_r[0], d))
+
+    return thing_rdicts
+
+
+# OLD:Returns the response converted into a dictionary
+# Old school synchronous function
 def request_dict(url, params):
     r = requests.get(url, params=params)
     
