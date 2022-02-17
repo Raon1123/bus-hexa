afc9f1e1a9598de256271f4d9ff63d85dfe5b27c
jaehwan1912 <jimshim@unist.ac.kr>
2022-02-17 15:31:13 +0900

    In development

--- bushexa/apibushexa/helper/get_busno.py
+++ bushexa/apibushexa/helper/get_busno.py
@@ -1,2 +1,62 @@
-def get_all_arrival():
-    return 
+from chroniccrawler.models import *
+
+
+'''
+def get_all_parts():
+    
+    parts = PartOfLane.objects.all().values()
+
+    lane_main = {}
+    for p in parts:
+        if lane_main
+
+    return PartOfLane.objects.all()
+'''
+
+def get_all_arrivals():
+    # We need : Lane primary key, remain_time, stop_name, vehicle_no, 
+    arrivals = UlsanBus_ArrivalInfo.objects.all().select_related('route_key_usb__route_key')
+
+    arrival_dicts = [
+        {
+            "pk": arr.route_key_usb.route_key.id,
+            "remain_time": arr.arrival_time,
+            "stop_name": arr.current_node_name,
+            "vehicle_no": arr.vehicle_no,
+        }
+        for arr in arrivals]
+
+    return arrival_dicts
+
+
+def get_all_positions():
+    # We need : Lane primary key, bus name, remain_time, stop name, vehicle_no,
+    positions = PosOfBus.objects.all().select_related('route_key')
+
+    position_dicts = [
+        {
+            "pk": pos.route_key.id,
+            "bus_name": pos.route_key.bus_name,
+            "remain_time": None,
+            "stop_name": None,
+            "vehicle_no": pos.bus_num,
+        }
+        for pos in positions]
+
+    return position_dicts
+
+
+def build_response_dict():
+    
+    # parts = get_all_parts()
+
+    rd ={
+            #"parts": str(type(list(parts.values())[0])),
+            "arrival": get_all_arrivals(),
+            "position": get_all_positions(),
+            "departure": {
+                "tbi": "tbi",
+            }
+        }
+
+    return rd
--- bushexa/apibushexa/views.py
+++ bushexa/apibushexa/views.py
@@ -5,13 +5,4 @@ from .helper import get_busno as get_busno
 
 
 def api_get_busno(request):
-    return JsonResponse(
-        {
-            "arrival": get_busno.get_all_arrival(),
-            "position": {
-                "tbi": ["tbi", "tbi", "tbi"],
-            },
-            "departure": {
-                "tbi": "tbi",
-            }
-        })
+    return JsonResponse(get_busno.build_response_dict())
